8c9cf5c1ddeb96356044fd81a09329566a2b03a6
本馬遼太郎 <rajiku.blanc@gmail.com>
2021-09-05 12:27:42 +0900

    shopifyからデータを取得して表示

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,36 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { Product } from 'shopify-buy'
+import { GetStaticProps } from 'next'
+import { client } from '../lib/client';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+type IndexProps = {
+  products: Product[];
+};
+
+const IndexPage: React.FC<IndexProps> = ({ products }) => {
+  return (
+    <>
+      <h1>Hello Next.js 👋</h1>
+      <ul>
+        {
+          products.map((product) =>
+          <li key={product.id}>
+            {product.title}
+            <img src={product.images[0].src} height={80}/>
+          </li>)
+        }
+      </ul>
+    </>
+  );
+}
 
 export default IndexPage
+
+export const getStaticProps: GetStaticProps = async () => {
+  const products: any = await client.product.fetchAll();
+  const data:any = JSON.parse(JSON.stringify(products));
+  return {
+    props: {
+      products: data
+    },
+  };
+};
